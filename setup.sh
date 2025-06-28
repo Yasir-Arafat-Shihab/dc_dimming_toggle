576803b40f3346648f1d2ebaf9646d31dbe9efd1
ehhhSenpai <170895179+ehhhSenpai@users.noreply.github.com>
2025-06-29 00:19:31 +0530

    add: setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+curl -o $HOME/dc_dimming_toggle.sh https://raw.githubusercontent.com/ehhhSenpai/dc_dimming_toggle/main/dc_dimming_toggle.sh
+mkdir -p $HOME/.shortcuts
+mv $HOME/dc_dimming_toggle.sh $HOME/.shortcuts/
+chmod +x $HOME/.shortcuts/dc_dimming_toggle.sh
+if pm list packages | grep -q "com.termux.widget"; then
+  echo "You may create a widget now"
+else
+  echo "Install termux_widget app and create a widget"
+fi
+exit 0
