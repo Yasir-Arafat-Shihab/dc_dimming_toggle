142b13f11a27a3adb9ed49b0270e9e2678bec330
Yasir-Arafat-Shihab <76649075+Yasir-Arafat-Shihab@users.noreply.github.com>
2025-07-05 13:34:06 +0600

    Dc_Dimming_toggle.sh

--- dc_dimming_toggle.sh
+++ dc_dimming_toggle.sh
@@ -3,55 +3,55 @@
 status="$(su -c cat /sys/kernel/oplus_display/dimlayer_bl_en)"
 
 if [[ "$status" == "0 0" ]]; then
-  sleep 0.1
-  echo "dc_dimming is currently DISABLED"
   sleep 0.2
+  echo "dc_dimming is currently DISABLED"
+  sleep 0.3
   echo "Enabling.."
   su -c 'echo "1 0" > /sys/kernel/oplus_display/dimlayer_bl_en'
   sleep 0.4
   echo "dc_dimming is now ENABLED"
-  sleep 0.2
-  read -n 1 -p "Reboot Now (y|n): " i
+  sleep 0.5
+  read -n 1 -p "Exit Now (y|n): " i
   echo
   if [[ -z "$i" ]]; then
-    echo "Nothing entered, reboot manually."
-    sleep 0.3
+    echo "Nothing entered, exit manually."
+    sleep 1.0
   elif [[ "$i" =~ ^[Yy]$ ]]; then
-    echo "Rebooting.."
-    sleep 0.3
-    su -c reboot
+    echo -e "Exiting..\n N.B. ☞ Rebooting will dismiss toggle"
+    sleep 1.0
+    su -c exit
   elif [[ "$i" =~ ^[Nn]$ ]]; then
-    echo "Okay, reboot manually."
-    sleep 0.3
+    echo "Okay, exit manually."
+    sleep 1.0
   else
-    echo "Wrong input, reboot manually."
-    sleep 0.3
+    echo "Wrong input, exit manually."
+    sleep 1.0
   fi
 
 elif [[ "$status" == "0 1" ]]; then
-  sleep 0.1
-  echo "dc_dimming is currently ENABLED"
   sleep 0.2
+  echo "dc_dimming is currently ENABLED"
+  sleep 0.3
   echo "Disabling.."
-  su -c 'echo "0 0" > /sys/kernel/oplus_display/dimlayer_bl_en'
+  su -c 'echo "1 0" > /sys/kernel/oplus_display/dimlayer_bl_en'
   sleep 0.4
   echo "dc_dimming is now DISABLED"
-  sleep 0.2
-  read -n 1 -p "Reboot Now (y|n): " i
+  sleep 0.5
+  read -n 1 -p "Exit Now (y|n): " i
   echo
   if [[ -z "$i" ]]; then
-    echo "Nothing entered, reboot manually."
-    sleep 0.3
+    echo "Nothing entered, exit manually."
+    sleep 1.0
   elif [[ "$i" =~ ^[Yy]$ ]]; then
-    echo "Rebooting.."
-    sleep 0.3
-    su -c reboot
+    echo -e "Exiting..\n N.B. ☞ Rebooting will dismiss toggle"
+    sleep 1.0
+    su -c exit
   elif [[ "$i" =~ ^[Nn]$ ]]; then
-    echo "Okay, reboot manually."
-    sleep 0.3
+    echo "Okay, exit manually."
+    sleep 1.0
   else
-    echo "Wrong input, reboot manually."
-    sleep 0.3
+    echo "Wrong input, exit manually."
+    sleep 1.0
   fi
 
 else
